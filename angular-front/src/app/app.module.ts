5fb4fb11145bd417b111ff9c80bf0f2c12b56919
sergio <sergioverde90@gmail.com>
2017-05-01 11:10:37 +0200

    remove old imports

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { MarkdownToHtmlModule } from 'ng2-markdown-to-html';
 import { AppComponent } from './app.component';
 import { EntryService } from 'app/entry-component/EntryService';
 import { MarkdownParserService } from 'app/MarkdownParserService';
-import { MarkdownModule } from 'angular2-markdown';
 
 @NgModule({
   declarations: [
@@ -14,8 +12,6 @@ import { MarkdownModule } from 'angular2-markdown';
   ],
   imports: [
     BrowserModule,
-    // MarkdownToHtmlModule.forRoot(),
-    MarkdownModule.forRoot(),
     FormsModule,
     HttpModule
   ],
