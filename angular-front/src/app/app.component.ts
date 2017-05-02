b2b0d285baa244225f9c0e2aef10c02b8af3c3cd
sergio <sergioverde90@gmail.com>
2017-05-02 21:06:01 +0200

    + highlight.js added

--- angular-front/src/app/app.component.ts
+++ angular-front/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import {Entry} from 'app/entry-component/Entry';
+import { Component, ViewChildren, ElementRef } from '@angular/core';
+import { Entry } from 'app/entry-component/Entry';
 import { OnInit } from '@angular/core';
-import {EntryService} from 'app/entry-component/EntryService';
+import { EntryService } from 'app/entry-component/EntryService';
 import { MarkdownParserService } from 'app/MarkdownParserService';
+
+declare var hljs: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl : './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
@@ -15,17 +17,30 @@ export class AppComponent implements OnInit {
 
     constructor(
       private entryService : EntryService,
-      private parser       : MarkdownParserService)
+      private parser       : MarkdownParserService,
+      private elementRef: ElementRef)
     { }
 
     ngOnInit(): void {
       this.entryService.fromJSON().
       subscribe(entries => {
         this.entries = entries;
-        for(let entry in this.entries){
-          let parsed = this.parser.convert(this.entries[entry].content);
-          this.entries[entry].content = parsed;
-        }
+        this.toMarkdown();
       });
+    }
+
+    highlight() {
+
+    }
+
+    ngAfterViewInit() {
+      hljs.highlightBlock(/** this.codeElement[0].nativeElement **/);
+    }
+
+    toMarkdown() {
+      for(let entry in this.entries){
+        let parsed = this.parser.convert(this.entries[entry].content);
+        this.entries[entry].content = parsed;
+      }
     }
 }
